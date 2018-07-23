7105d9e6f27e0d44ac6181e2efd1ff667978ff99
Josh Kincheloe <jkincheloe33@gmail.com>
2018-07-23 11:57:10 -0500

    Added hero section and styles

--- gulpfile.js
+++ gulpfile.js
@@ -9,56 +9,56 @@ var babel = require('gulp-babel');
 var rename = require('gulp-rename');
 
 // default when you run gulp
-gulp.task('default', ['styles', 'scripts', 'lint'], function() {
-  console.log('What? SUPPORT is evolving!');
-  gulp.watch('sass/**/*.sass', ['styles']);
-  gulp.watch('js/**/*.js', ['scripts', 'lint']);
-  gulp.watch('./index.html').on('change', browserSync.reload);
-  browserSync.init({
-    server: './',
-    open: false,
-    logPrefix: 'JLB Support'
-  });
+gulp.task('default', ['styles', 'scripts', 'lint'], function () {
+    console.log('What? SUPPORT is evolving!');
+    gulp.watch('sass/**/*.sass', ['styles']);
+    gulp.watch('js/**/*.js', ['scripts', 'lint']);
+    gulp.watch('./index.html').on('change', browserSync.reload);
+    browserSync.init({
+        server: './',
+        open: false,
+        logPrefix: 'JLB Support'
+    });
 });
 
 // sass task
-gulp.task('styles', function() {
-  gulp.src('sass/**/main.sass')
-    // compress & minify
-    .pipe(sass({
-      outputStyle: 'compressed'
-    }).on('error', sass.logError))
-    // add autoprefixers
-    .pipe(autoprefixer({
-      browsers: ['last 2 versions']
-    }))
-    // rename file
-    .pipe(rename({
-      basename: 'styles',
-      extname: '.min.css'
-    }))
-    .pipe(gulp.dest('./minified/'))
-    .pipe(browserSync.stream());
+gulp.task('styles', function () {
+    gulp.src('sass/**/main.scss')
+        // compress & minify
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }).on('error', sass.logError))
+        // add autoprefixers
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions']
+        }))
+        // rename file
+        .pipe(rename({
+            basename: 'styles',
+            extname: '.min.css'
+        }))
+        .pipe(gulp.dest('./minified/'))
+        .pipe(browserSync.stream());
 });
 
 // scripts task including es6 syntax
-gulp.task('scripts', function() {
-  gulp.src('js/**/*.js')
-    .pipe(babel({
-      presets: ['env']
-    }))
-    .pipe(uglify())
-    .pipe(rename({
-      basename: 'scripts',
-      extname: '.min.js'
-    }))
-    .pipe(gulp.dest('./minified/'));
+gulp.task('scripts', function () {
+    gulp.src('js/**/*.js')
+        .pipe(babel({
+            presets: ['env']
+        }))
+        .pipe(uglify())
+        .pipe(rename({
+            basename: 'scripts',
+            extname: '.min.js'
+        }))
+        .pipe(gulp.dest('./minified/'));
 });
 
 // lint task to check syntax
-gulp.task('lint', function() {
-  return gulp.src(['js/**/*.js'])
-    .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(eslint.failOnError());
+gulp.task('lint', function () {
+    return gulp.src(['js/**/*.js'])
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failOnError());
 });
