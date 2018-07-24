f3e0c7f01216ba0060aeb02aae0aa6585cfa29ce
Josh Kincheloe <jkincheloe33@gmail.com>
2018-07-23 20:10:33 -0500

    Added animation while scrolling.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,34 @@
 $(document).ready(function () {
 
-    console.log('Hello World');
+    let scroll = 0;
+
+    $(window).scroll(() => {
+
+        let scrollY = window.scrollY;
+        if (scroll > 120 && scrollY > 120) {
+            return;
+        } else if (scroll <= 120 && scrollY <= 120) {
+
+            return;
+        }
+
+        scroll = scrollY;
+        // console.log(scroll);
+        if (scroll > 120) {
+            $('#collageOne').addClass('fadeInUp load');
+            $('#collageTwo').addClass('fadeInLeft load');
+            $('#collageThree').addClass('fadeInLeft load');
+            $('#collageFour').addClass('fadeInDown load');
+            $('#collageFive').addClass('fadeInUp load');
+
+        } else {
+            $('#collageOne').removeClass('fadeInUp load');
+            $('#collageTwo').removeClass('fadeInLeft load');
+            $('#collageThree').removeClass('fadeInLeft load');
+            $('#collageFour').removeClass('fadeInDown load');
+            $('#collageFive').removeClass('fadeInUp load');
+        }
+
+    });
 
 });
